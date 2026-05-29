81e738ff5d3c77c5c77718b310f0e9f27e818fbf
Ali <alilafi789@gmail.com>
2026-05-30 01:51:09 +0300

    Add admin page with create form and invalidate cache on create

--- web/src/api/songs.ts
+++ web/src/api/songs.ts
@@ -15,12 +15,35 @@ export type SongsResponse={
     pageSize: number;
 }
 
+export type CreateSongInput ={
+    name:string;
+    singer: string;
+    coverImageUrl?:string;
+    album?:string;
+    duration?:string;
+}
+
 export async function getSongs(params:{pageNumber:number; pageSize:number; search?:string}): Promise<SongsResponse>{
     const searchParams = new URLSearchParams({pageNumber: String(params.pageNumber), pageSize: String(params.pageSize)});
     if(params.search) searchParams.set('search', params.search)
     
     const response = await fetch(`http://localhost:3000/api/songs?${searchParams.toString()}`);
     if(!response.ok) throw new Error('Failed to fetch songs');
+
+    return response.json();
+}
+
+export async function createSong(input:CreateSongInput):Promise<Song>{
+    const token = localStorage.getItem('accessToken');
+    const response = await fetch('http://localhost:3000/api/songs',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(input)
+    });
+    if(!response.ok) throw new Error('Failed to create song');
 
     return response.json();
 }
--- web/src/pages/admin-page.tsx
+++ web/src/pages/admin-page.tsx
@@ -1,3 +1,43 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react"
+import { createSong } from "../api/songs";
+
 export function AdminPage(){
-    return <h1>Admin</h1>
+
+    const [name, setName] =useState('');
+    const [singer, setSinger] = useState('');
+    const [album, setAlbum] = useState('');
+    const queryClient = useQueryClient();
+
+    const createSongMutation = useMutation({
+        mutationFn:createSong,
+        onSuccess:()=>{
+            setName('');
+            setSinger('');
+            setAlbum('');
+            queryClient.invalidateQueries({queryKey:['songs']})
+        }
+    })
+    
+    return (<div>
+        <h1>Add Song</h1>
+        <form onSubmit={(event) =>{
+            event.preventDefault();
+            createSongMutation.mutate({name,singer,album});
+        }}>
+            <label>Name</label>
+            <input value={name} onChange={(event)=> setName(event.target.value)}/>
+            
+            <label>Singer</label>
+            <input value={singer} onChange={(event)=> setSinger(event.target.value)}/>
+
+            <label>Album</label>
+            <input value={album} onChange={(event)=> setAlbum(event.target.value)}/>
+            <button type="submit" disabled={createSongMutation.isPending}> {createSongMutation.isPending? 'Creating...': 'Create Song'}</button>
+        </form>
+
+        {createSongMutation.isSuccess && <p>Song Created Successfully !!!</p>}
+        {createSongMutation.isError && <p>Failed to create song</p>}
+
+    </div>)
 }
